c0df87e00d20978b54557ec2ff4ef71763a07150
KVKrishna <krishnakvbharadwaj@gmail.com>
2023-08-08 19:21:29 +0530

    New change by SDET1

--- src/test/java/SDET2.java
+++ src/test/java/SDET2.java
@@ -7,7 +7,7 @@ public class SDET2 {
 		System.out.println("This is SDET2 Test case");
 		//New Line added FOR TESTING by SDET2
 	
-	//Changes done by SDET1 
+	//Changes done by SDET1 and added some Tests
 		
 	}
 
