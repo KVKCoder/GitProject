af45240a627f91167b77d0cd07359aa58918ede0
KVKrishna <krishnakvbharadwaj@gmail.com>
2023-08-08 18:27:40 +0530

    New Line added FOR TESTING by SDET2

---
+++ src/test/java/SDET2.java
@@ -0,0 +1,11 @@
+
+public class SDET2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is SDET2 Test case");
+		//New Line added FOR TESTING by SDET2
+	}
+
+}
